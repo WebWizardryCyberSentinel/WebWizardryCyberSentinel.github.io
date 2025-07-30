1ce2f1da5f5ac745b391f22ad584c7b02cff112b
Martin van Tongerlo <Martinvantongerlo@gmail.com>
2025-07-30 14:11:11 +0100

    anime library changes

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,4 +1,4 @@
-import { default as anime } from '../scripts/animejs/lib/anime.es.js';
+import { anime } from '../scripts/animejs/lib/anime.es.js';
 
 document.addEventListener("DOMContentLoaded", function()  {
 
